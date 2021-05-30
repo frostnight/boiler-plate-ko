85a4db9c8eb7c716c786b19d9242107594b87d2c
frostnight <frostnightsong@gmail.com>
2021-05-30 17:57:02 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!~~')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   // 회원 가입할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터베이스에 넣어준다.
 
@@ -65,7 +65,20 @@ app.post('/login', (req, res) => {
       })
     })
   })
-  // 비밀번호 까지 맞다면 토큰을 생성하기.
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+  // 미들웨어를 통과해서 Authentication이 True
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role == 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
 })
 
 app.listen(port, () => {
