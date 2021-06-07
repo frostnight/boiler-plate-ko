56fcb0316f5aa7d9623ac7dd714b9ee56bb5c580
frostnight <frostnightsong@gmail.com>
2021-06-07 16:43:29 +0900

    회원가입 기능 개발

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER
 } from './types';
 
 export function loginUser(dataToSumbit) {
@@ -10,6 +11,17 @@ export function loginUser(dataToSumbit) {
 
     return {
         type: LOGIN_USER,
+        payload : request
+    }
+}
+
+export function registerUser(dataToSumbit) {
+    
+    const request = axios.post('/api/users/register', dataToSumbit)
+    .then(response => response.data)
+
+    return {
+        type: REGISTER_USER,
         payload : request
     }
 }
